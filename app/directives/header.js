079e0016542f73eb3f4ac6d52f5bbabb8dbc4960
root <root@chef-node1.c2clab.com>
2017-08-21 17:29:49 +0530

    first commit

---
+++ app/directives/header.js
@@ -0,0 +1,13 @@
+angular
+.module('dockm')
+.directive('rdHeader', function rdHeader() {
+  var directive = {
+    scope: {
+      'ngModel': '='
+    },
+    transclude: true,
+    template: '<div class="row header"><div class="col-xs-12"><div class="meta" ng-transclude></div></div></div>',
+    restrict: 'EA'
+  };
+  return directive;
+});
