28b0f66ac7c7d844fb5263030020d359359ad830
aniket.pawar <aniket.pawar@click2cloud.net>
2017-09-13 11:38:36 +0530

    UI fixes for sidebar alignment.

--- app/directives/header-title.js
+++ app/directives/header-title.js
@@ -14,9 +14,9 @@ angular
     template: '<div class="row">' +
     '            <div class="col-md-12">' +
     '                <b class="topTile">{{title}}</b> ' +
-    '                <span class="pull-right user-box" style="padding-left: 3%; padding-top: 3px;" ng-if="username"><i class="fa fa-user-circle" aria-hidden="true"></i> {{username}}</span> ' +
     '                <span class="header_title_content" ng-transclude></span> '+
     '                <a href="" title="Menu" class="hamburger animated fadeInUp delay fa fa-bars"> </a> ' +
+    '                <span class="pull-right user-box" style="padding-right: 7%; padding-top: 2px" ng-if="username"><i class="fa fa-user-circle" aria-hidden="true"></i> {{username}}</span> ' +
     '            </div>' +
     '        </div>',
     restrict: 'E'
